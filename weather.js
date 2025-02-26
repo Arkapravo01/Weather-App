412b43ad58f9a5e73918219f37ea5c6faee28dc5
Arkapravo <arkapravochakrabarti188@gmail.com>
2025-02-26 17:53:27 +0530

    Initital Commit

---
+++ weather.js
@@ -0,0 +1,26 @@
+const input=document.getElementById('search')
+const btn=document.getElementById('btn')
+
+const cityName=document.getElementById('cityName')
+const cityTemp=document.getElementById('temp')
+const cityTime=document.getElementById('time')
+const img=document.getElementById('img')
+
+// http://api.weatherapi.com/v1/current.json?key=156f06ec9bb54731967110553252602&q=Kolkata&aqi=yes //
+
+async function getData(myInputData) {
+    const data=await fetch(`http://api.weatherapi.com/v1/current.json?key=156f06ec9bb54731967110553252602&q=${myInputData}&aqi=yes`)
+    return data.json()
+}
+
+btn.addEventListener('click',async()=>{
+    const myInputData=input.value
+    console.log(myInputData);
+    const result=await getData(myInputData)
+    console.log(result.Location);
+
+    cityName.innerText=result.location.region
+    cityTemp.innerText=result.current.temp_c
+    cityTime.innerText=result.location.localtime
+    img.innerHTML=`<img src="${result.current.condition.icon}" alt="Image"/>`
+})
